121189816639b82fd9b7aa6bbf046b9f0a1df533
Jessie-Wilkins <wilkinsj@fontbonne.edu>
2017-12-08 18:04:39 -0600

    New and improved sql dump file.

--- project_db_SQLdump.sql
+++ project_db_SQLdump.sql
@@ -3,24 +3,19 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 10, 2017 at 08:13 PM
+-- Generation Time: Dec 09, 2017 at 12:56 AM
 -- Server version: 10.1.26-MariaDB
--- 
-PHP Version: 7.1.9
+-- PHP Version: 7.1.9
 
---
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
-
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-
 /*!40101 SET NAMES utf8mb4 */;
 
 --
@@ -30,8 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- 
-Table structure for table `customer`
+-- Table structure for table `customer`
 --
 
 CREATE TABLE `customer` (
@@ -45,11 +39,19 @@ CREATE TABLE `customer` (
   `Preference` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`Fname`, `Lname`, `CellNum`, `Email`, `NotifyFreq`, `ReasonForVisit`, `TicketID`, `Preference`) VALUES
+('Jessie', 'Wilkins', 911, 'ProbablySomethingRelatedToVideoGames@gmail.com', '5', 'Billing', 'VWXYZ123456789', 1),
+('Samantha', 'Eidson', 12345, 'SomethingAboutChickensAndDatabases@gmail.com', '5', 'Billing', 'QWERT123456789', 1),
+('Yuri', 'Khechoyan', 2147483647, 'SomethingWithPandasInIt@gmail.com', '5', 'Phone_Repair', 'ABCDE123456789', 0);
+
 -- --------------------------------------------------------
 
 --
--- 
-Table structure for table `device`
+-- Table structure for table `device`
 --
 
 CREATE TABLE `device` (
@@ -62,8 +64,7 @@ CREATE TABLE `device` (
 -- --------------------------------------------------------
 
 --
--- 
-Table structure for table `employee`
+-- Table structure for table `employee`
 --
 
 CREATE TABLE `employee` (
@@ -74,32 +75,48 @@ CREATE TABLE `employee` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`EMPID`, `ticket_assigned`, `fname`, `lname`) VALUES
+('A123456789', 'QWERT123456789', 'Steven', 'Olson'),
+('B123456789', 'ABCDE123456789', 'John', 'McQuaide');
+
+--
 -- Indexes for dumped tables
 --
 
 --
--- 
-Indexes for table `device`
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`CellNum`),
+  ADD KEY `FK_TicketConstraint` (`TicketID`);
+
+--
+-- Indexes for table `device`
 --
 ALTER TABLE `device`
   ADD PRIMARY KEY (`Serial_Number`);
 
 --
--- 
-Indexes for table `employee`
+-- Indexes for table `employee`
 --
 ALTER TABLE `employee`
-  ADD PRIMARY KEY (`EMPID`);
+  ADD PRIMARY KEY (`EMPID`),
+  ADD KEY `ticket_assigned` (`ticket_assigned`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `customer`
+--
+ALTER TABLE `customer`
+  ADD CONSTRAINT `FK_TicketConstraint` FOREIGN KEY (`TicketID`) REFERENCES `employee` (`ticket_assigned`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-
-
-
-ALTER TABLE customer
-ADD CONSTRAINT FK_TicketConstraint
-FOREIGN KEY (TicketID) REFERENCES employee(ticket_assigned);
